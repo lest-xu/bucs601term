989dd645ce036abce69348c6cbdcfa46c44b1d2a
lest-xu <autodataxx@gmail.com>
2024-06-17 10:25:20 -0400

    update

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -11,7 +11,7 @@ class Resume extends React.Component {
                             <h2>Resume</h2>
                             <p>Explore my professional background through my resume. It covers my skills, experiences, education, and key projects.</p>
                             <p>For a downloadable version, please click the download button.</p>
-                            <iframe title='pdffile' src='https://file-examples.com/wp-content/storage/2017/10/file-sample_150kB.pdf' width='100%' height='500px' />
+                            <iframe title='pdffile' src='https://lest-xu.github.io/LiXu-BUResume.pdf' width='100%' height='500px' />
 
                         </div>
                     </div>
